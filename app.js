2ff81101b1c9299d6dcf18619b917f68cb5b78e8
Nguyen Dinh Quoc <nguyendq@pascaliaasia.com>
2021-03-14 13:49:58 +0700

    adding routes folder middleware

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-const routes = require("./routes");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-const server = http.createServer(routes);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(9001);
+// midleware
+// app.use("/add-product", (req, res, next) => {
+//   res.send(
+//     "<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add product</button></form>"
+//   );
+//   // don't call next() here, it will fire the '/' midleware
+// });
+
+// // midleware for POST method only
+// app.post("/product", (req, res, next) => {
+//   console.log(req.body);
+//   res.redirect("/");
+// });
+
+app.use("/admin", adminRoutes);
+
+// app.use("/", (req, res, next) => {
+//   res.send("<h1>Hello from express</h1>");
+// });
+app.use(shopRoutes);
+
+// add 404
+app.use((req, res, next) => {
+  res.send("<h1>Page not found</h1>").status(404);
+});
+app.listen(3000);
