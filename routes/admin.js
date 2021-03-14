1bbc58d59b96c46e16aad29b1d19876cc1a2dd85
quangkhuat <quangkhuat1995@gmail.com>
2021-03-14 15:58:11 +0700

    pug:view engine

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,26 @@
 const path = require("path");
+
 const express = require("express");
 
 const rootDir = require("../util/path");
+
 const router = express.Router();
 
+const products = [];
+
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  // don't call next() here, it will fire the '/' midleware
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
-// midleware for POST method only
+// /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,15 @@
 const path = require("path");
-const rootDir = require("../util/path");
 
 const express = require("express");
+
+const rootDir = require("../util/path");
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // dirname point to routes folder so we must add ../
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop haha", path: "/" });
 });
 
 module.exports = router;
