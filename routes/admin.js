4645377d8b1de9b56bb5f0672b8c71ada73b0219
quangkhuat <quangkhuat1995@gmail.com>
2021-03-14 15:22:31 +0700

    fix typo

--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,13 @@ const express = require("express");
 const rootDir = require("../util/path");
 const router = express.Router();
 
-router.use("/add-product", (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
   // don't call next() here, it will fire the '/' midleware
 });
 
 // midleware for POST method only
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
