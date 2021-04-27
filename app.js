0cdb7c9a8070463201d0dbe6bc66fb0891df8a8a
quangkhuat <quangkhuat1995@gmail.com>
2021-04-27 13:30:15 +0700

    cookies-session, fix layout, add login form

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set("view engine", "ejs");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 const errorControllers = require("./controllers/error");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -42,6 +43,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 // add 404
 app.use(errorControllers.get404);
