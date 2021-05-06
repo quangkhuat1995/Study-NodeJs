2c3eb62cd556d2e1abf7b186c4a0068da008a61a
quangkhuat <quangkhuat1995@gmail.com>
2021-05-06 16:31:54 +0700

    adding pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit");
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
@@ -38,7 +40,11 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const { page } = req.query;
+
   Product.find()
+    .skip((Number(page || 1) - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then((products) => {
       res.render("shop/index", {
         // <= path to folder views
